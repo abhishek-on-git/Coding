142243506c72d03842766412cab40bfcb3a6bc2a
abhishek-on-git <me.abhishek92@gmail.com>
2025-06-10 15:30:19 +0530

    Solution for Pascal's triangle 2

--- src/main/kotlin/DynamicProgramming/PascalsTriangle.kt
+++ src/main/kotlin/DynamicProgramming/PascalsTriangle.kt
@@ -4,6 +4,11 @@ class PascalsTriangle {
 
     // Pascal's Triangle 1 (LC 118)
     /**
+     *    1
+     *   1 1
+     *  1 2 1
+     * 1 3 3 1
+     *
      * Why is this a DP problem?  Because we construct current row based on previous row,
      * which is an already solved problem.
      */
@@ -23,6 +28,27 @@ class PascalsTriangle {
                 prev = row
             }
             return ans
+        }
+    }
+
+    // Pascal's Triangle 2 (LC 119)
+    /**
+     * Only difference from Pascal's Triangle 1 is that here we
+     * are asked to return just the ith row, not all the rows
+     */
+    class Pascal2Solution {
+        fun getRow(rowIndex: Int): List<Int> {
+            var prev = mutableListOf(1)
+            for(i in 1..rowIndex) {
+                val row = mutableListOf<Int>()
+                row.add(1)
+                for(j in 0 until i-1) {
+                    row.add(prev[j] + prev[j+1])
+                }
+                row.add(1)
+                prev = row
+            }
+            return prev
         }
     }
 }
