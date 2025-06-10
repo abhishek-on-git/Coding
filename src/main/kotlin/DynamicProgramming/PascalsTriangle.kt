598040175aa709691472040fecb53aaf05b7808a
abhishek-on-git <me.abhishek92@gmail.com>
2025-06-10 13:55:50 +0530

    Pascals Triangle Solution 1

---
+++ src/main/kotlin/DynamicProgramming/PascalsTriangle.kt
@@ -0,0 +1,24 @@
+package org.example.DynamicProgramming
+
+class PascalsTriangle {
+
+    // Pascal's Triangle 1 (LC 118)
+    class Pascal1Solution {
+        fun generate(numRows: Int): List<List<Int>> {
+            val ans = mutableListOf<List<Int>>()
+            var prev = listOf(1)
+            ans.add(prev)
+            for(i in 1 until numRows) {
+                val row = mutableListOf<Int>()
+                row.add(1)
+                for(j in 0 until i - 1) {
+                    row.add(prev[j] + prev[j+1])
+                }
+                row.add(1)
+                ans.add(row)
+                prev = row
+            }
+            return ans
+        }
+    }
+}
