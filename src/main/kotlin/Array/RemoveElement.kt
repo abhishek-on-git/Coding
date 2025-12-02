9d061fc3f89965f22e0d5b6e88550097a693ed9f
abhishek-on-git <me.abhishek92@gmail.com>
2025-12-02 15:29:57 +0530

    Solution for Remove Element O(N)

---
+++ src/main/kotlin/Array/RemoveElement.kt
@@ -0,0 +1,27 @@
+package org.example.Array
+
+class RemoveElement {
+
+    /**
+     * Leetcode 27 - https://leetcode.com/problems/remove-element/description
+     * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+     * The order of the elements may be changed.
+     * Then return the number of elements in nums which are not equal to val.
+     * Consider the number of elements in nums which are not equal to val be k, to get accepted,
+     * you need to do the following things:
+     * 1. Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+     *    The remaining elements of nums are not important as well as the size of nums.
+     * 2. Return k.
+     */
+    class Solution {
+        fun removeElement(nums: IntArray, `val`: Int): Int {
+            var insert = 0
+            for(i in nums.indices) {
+                if(nums[i] != `val`) {
+                    nums[insert++] = nums[i]
+                }
+            }
+            return insert
+        }
+    }
+}
