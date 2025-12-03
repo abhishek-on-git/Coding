35db62538955fc9dbc79551cf5688f47e66e2906
abhishek-on-git <me.abhishek92@gmail.com>
2025-12-03 14:40:21 +0530

    Solution for Contains Duplicate II O(N)

---
+++ src/main/kotlin/Array/ContainsDuplicate_II.kt
@@ -0,0 +1,22 @@
+package org.example.Array
+
+class ContainsDuplicate_II {
+
+    /**
+     * Leetcoce 219 - https://leetcode.com/problems/contains-duplicate-ii/description
+     * Given an integer array nums and an integer k, return true if there are
+     * two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+     */
+    class Solution {
+        fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+            val map = mutableMapOf<Int, Int>()
+            for(i in nums.indices) {
+                if(map.containsKey(nums[i]) && Math.abs(map.getValue(nums[i]) - i) <= k) {
+                    return true
+                }
+                map[nums[i]] = i
+            }
+            return false
+        }
+    }
+}
