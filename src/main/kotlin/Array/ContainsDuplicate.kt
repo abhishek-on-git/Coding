4ae33fff16384e72d8f3bf8317bfe5de1bfd337c
abhishek-on-git <me.abhishek92@gmail.com>
2025-12-03 14:11:24 +0530

    Solution for Contains Duplicate O(N)

---
+++ src/main/kotlin/Array/ContainsDuplicate.kt
@@ -0,0 +1,19 @@
+package org.example.Array
+
+class ContainsDuplicate {
+    /**
+     * Leetcode 217 - https://leetcode.com/problems/contains-duplicate/description
+     * Given an integer array nums, return true if any value appears at least twice in the array,
+     * and return false if every element is distinct.
+     */
+    class Solution {
+        fun containsDuplicate(nums: IntArray): Boolean {
+            val set = mutableSetOf<Int>()
+            for(num in nums) {
+                if(set.contains(num)) return true
+                set.add(num)
+            }
+            return false
+        }
+    }
+}
