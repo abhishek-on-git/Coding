52f269dc1bdd7ab70caee949c9d90177e13d1111
abhishek-on-git <me.abhishek92@gmail.com>
2025-12-21 14:30:43 +0530

    Solution for 3696 Maximum distance between unequal words I

---
+++ src/main/kotlin/Array/MaxDistanceUnequalWords.kt
@@ -0,0 +1,33 @@
+package org.example.Array
+
+/**
+ * Leetcode 3696 - Maximum distance between unequal words I
+ * You are given a string array words.
+ * Find the maximum distance between two distinct indices i and j such that:
+ * words[i] != words[j], and
+ * the distance is defined as j - i + 1.
+ * Return the maximum distance among all such pairs. If no valid pair exists, return 0.
+ */
+class MaxDistanceUnequalWords {
+
+    // Single pass solution.
+    // Traverse through the array and compare each word to the first and last words.
+    // If unequal, update max if this word's distance from first and last is higher than current max.
+    class Solution {
+        fun maxDistance(words: Array<String>): Int {
+            var max = 0
+
+            for(i in words.indices) {
+                if(words[0] != words[i]) {
+                    max = maxOf(max, i + 1)
+                }
+
+                if(words[i] != words[words.size-1]) {
+                    max = maxOf(max, words.size - i)
+                }
+            }
+
+            return max
+        }
+    }
+}
